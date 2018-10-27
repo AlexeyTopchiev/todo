2df45fd93f4f70bc516ba57f3c3e308055914fe6
AlexeyTopchiev <wearelols001@gmail.com>
2018-10-27 20:14:09 +0300

    Create react element-tutor

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+const el = (
+    <div>
+        <h1>My Todo List</h1>
+        <input placeholder="search" />
+        <ul>
+            <li>Learn React</li>
+            <li>Create Awesome App</li>
+        </ul>
+    </div>
+)
+
+// const el = React.createElement('h1', null, 'Hello World')
+
+ReactDOM.render(el, document.getElementById('root'))
