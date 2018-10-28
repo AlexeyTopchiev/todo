7be5e8190c24b1488bfb6e7d18c855d9f0ae5678
AlexeyTopchiev <wearelols001@gmail.com>
2018-10-28 18:03:17 +0300

    props of components-tutor

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,10 +5,14 @@ import TodoListItem from './TodoListItem'
 const TodoList = () => {
   return (
       <ul>
-          <li><TodoListItem /></li>
-          <li><TodoListItem /></li>
+          <li><TodoListItem label="Drink Coffee" /></li>
+          <li><TodoListItem label="Build React App" important /></li>
       </ul>
   )
 }
 
 export default TodoList
+
+// Мы можем передать любые свойства в любые Реакт-компоненты 
+// Передавать в качестве свойства другому компоненту можно любые значения 
+// (любой тип JS, строки, булевы, функции, массивы, объекты и тд)
