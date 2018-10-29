42ec7b9171a1cf19965322409eb824664e08ff41
AlexeyTopchiev <wearelols001@gmail.com>
2018-10-30 02:58:49 +0300

    import css-tutor

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,37 +1,25 @@
 import React from 'react'
-
 import TodoListItem from './TodoListItem'
+import './TodoList.css'
 
 const TodoList = ({ todos }) => {
 
     const elements = todos.map((item) => {
 
         const { id, ...itemProps } = item
-        
+
         return (
-            <li key={ id }>
+            <li key={ id } className="list-group-item">
             <TodoListItem { ...itemProps } />
             </li>
-            // ПЕРВЫЙ ВАРИАНТ (деструктуризировать, чтобы достать id и исп-ть rest-параметр,
-            // чтобы сохранить оставшиеся свойства в itemProps)
         )
     })
     
   return (
-      <ul>
+      <ul className="list-group todo-list">
         { elements }
       </ul>
   )
 }
 
 export default TodoList
-
-            //  ВТОРОЙ ВАРИАНТ (но в TodoListItem передаем лишнее свойство id)
-            // <li key={ item.id }> 
-            // <TodoListItem { ...item } />
-            // </li>
-            // ТРЕТИЙ ВАРИАНТ (самый простой)
-            // <li key={ item.id }> 
-            // <TodoListItem label={ item.label }
-            //               important={ item.important } />
-            // </li>
